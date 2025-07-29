4dad8e0a6607c082f1cc04540daab491fa23aa3c
Christophe Foyer <christophe.aa.foyer@gmail.com>
2025-07-29 15:29:53 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+export default {
+  srcDir: 'src',
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/',
